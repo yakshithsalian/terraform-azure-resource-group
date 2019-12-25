d61a81799d0e03662cfd87272df1ebb52d76fdb6
Sergio Sisternes <sesispla@outlook.com>
2019-12-25 13:39:15 +0100

    Fixed type warning

--- variables.tf
+++ variables.tf
@@ -1,27 +1,27 @@
 variable "name" {
-  type = "string"
+  type = string
   description = "(Required) The name of the resource group. Must be unique on your Azure subscription"
 }
 
 variable "location" {
-  type = "string"
+  type = string
   description = "(Required) The location where the resource group should be created. For a list of all Azure locations, please consult this link or run az account list-locations --output table."
 }
 
 variable "app" {
-  type = "string"
+  type = string
   description = "(Optional) Adds a tag with the application name this resource group belogs to."
   default = ""
 }
 
 variable "environment" {
-  type = "string"
+  type = string
   description = "(Optional) Environment name. If not specified, this module will use workspace as default value"
   default = "default"
 }
 
 variable "creator" {
-  type = "string"
+  type = string
   description = "(Optional) Adds a tag indicating the creator of this resource"
   default = "cloudcommons"
 }
