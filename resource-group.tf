35e59f548feaf2f19de5bc59247fa071cf2f83ed
Sergio Sisternes <sesispla@outlook.com>
2019-12-26 01:23:06 +0100

    Warning fixes

--- resource-group.tf
+++ resource-group.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "cloudcommons" {
-  name     = "${var.name}"
-  location = "${var.location}"
+  name     = var.name
+  location = var.location
 
   tags = {
-    app         = "${var.app}"
-    environment = "${var.environment}"
-    creator     = "${var.creator}"
+    app         = var.app
+    environment = var.environment
+    creator     = var.creator
   }
 }
